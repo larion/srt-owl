5b4ceccdd3a3b56a2cc4c077c48d3eccc4a48767
larion <lariongaraczi@gmail.com>
2015-02-08 17:13:08 +0100

    srt-owl and build-db refactored
    - Broke up very long functions.
    - We now use "use constant" instead of my $CONSTANT
    - pressing C in interactive mode shows all available contexts now, not just the last one.

--- build-db.pl
+++ build-db.pl
@@ -7,66 +7,86 @@
 
 use 5.018;
 use warnings;
-
 use DBI;
 
-my $F_NAME = "freqlists/nl.txt";
-my $DB_NAME = "srt-owl.db";
+use constant F_NAME  => "freqlists/nl.txt";
+use constant DB_NAME => "srt-owl.db";
 
-my $freqlist_fname = "$F_NAME";
 #my $freqlist_fname = "freqlists/$options{'lang'}.txt";
-open my $freq_list_fh, '<', $freqlist_fname or
+open my $freq_list_fh, '<', F_NAME or
   die "Can't open language file: $!";
 
-# process freq_list
+##### process freq_list #####
 # TODO more extensive word properties!!
 
 say "Building Frequency list";
 
-my %freqlist; # map words to their place in the frequency lists
-my($i, $total_occurences);
-for(<$freq_list_fh>){
-	chomp;
-	$i++; # position in frequency list
-	my($word, $occurences) = split;
-	my $total_occurences+=$occurences;
-	$freqlist{$word} = $i;
-}
+my $freqlist = {};
+build_freqlist($freqlist, $freq_list_fh);
 
-# connect to the database
+##### connect to the database #####
 
 say "Connecting to Database";
 
 my $dbh = DBI->connect(
-	"dbi:SQLite:$DB_NAME",
+	sprintf("dbi:SQLite:%s", DB_NAME),
 	"", # no username
 	"", # no password
 	{PrintError => 0, RaiseError => 1, AutoCommit => 0}
 ) or die "Can't connect to database: $DBI::errstr\n";
-
 $dbh->do("PRAGMA synchronous = OFF; PRAGMA journal_mode = WAL;"); # speed-up!
 
-# create the words table
+##### create the words table #####
 
 say "Creating tables";
+create_words_table($dbh, "nl"); # TODO
 
-my $sql_create_table =<<"SQL";
-CREATE TABLE IF NOT EXISTS words (
-	id  INTEGER PRIMARY KEY,
-	word  VARCHAR(255) NOT NULL); 
-SQL
-$dbh->do($sql_create_table);
-
-# populate the words table
+##### populate the words table #####
 
 say "Populating words table with the frequency list";
+freqlist2db($dbh, $freqlist);
 
-my $sql_insert = "INSERT INTO words (id, word) VALUES (?, ?);";
-my $sth = $dbh->prepare($sql_insert);
-my($id, $word);
-$sth -> execute($id, $word) while( ($word,$id) = each(%freqlist) );
+##### create the index #####
+
+say "Creating index";
+
+my $sql_select = "CREATE UNIQUE INDEX word_ind ON words (word)";
+$dbh->do($sql_select);
 $dbh->commit;
 
-# disconnect from DB
+##### disconnect from DB #####
 
 $dbh->disconnect;
+
+##############################
+
+sub build_freqlist { # map words to their place in the frequency lists
+	my($freqlist, $freqlist_fh) = @_;
+	my($i, $total_occurences);
+	for(<$freqlist_fh>){
+		chomp;
+		$i++; # position in frequency list
+		my($word, $occurences) = split;
+		my $total_occurences+=$occurences;
+		$freqlist->{$word} = $i;
+	}
+}
+
+sub create_words_table { #TODO
+	my($dbh, $lang) = @_;
+	my $sql_create_table =<<"SQL";
+CREATE TABLE IF NOT EXISTS words (
+	id  INTEGER PRIMARY KEY,
+	word  VARCHAR(255) NOT NULL);
+SQL
+	$dbh->do($sql_create_table);
+}
+
+sub freqlist2db {
+	my($dbh, $freqlist) = @_;
+	my $sql_insert = "INSERT INTO words (id, word) VALUES (?, ?);";
+	my $sth = $dbh->prepare($sql_insert);
+	my($id, $word);
+	$sth -> execute($id, $word) while( ($word,$id) = each(%$freqlist) );
+	$dbh->commit;
+}
